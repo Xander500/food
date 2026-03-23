1bde6551cfa8438195aeed24e4ee3d12569bef41
riderle <lrider@mail.umw.edu>
2026-03-23 09:11:08 -0400

    Revert "Accessibility focus formating (:focus-visible)"
    This reverts commit 31cd58bfa7df9ee1d08137f93e6c7ab46e25ecd5.

--- header.php
+++ header.php
@@ -66,7 +66,7 @@ if (date("H:i:s") > "18:19:59") {
             /* border-bottom-right-radius: 50px; */
         }
          .content-box-test:hover,
-         .content-box-test:focus-visible {
+         .content-box-test:focus {
             border: 4px solid var(--page-font-color);
         }
 /*END STYLE TEST*/
@@ -261,25 +261,14 @@ if (date("H:i:s") > "18:19:59") {
             transition: background 0.3s;
         }
         .dropdown div:hover,
-        .dropdown div:focus-visible {
+        .dropdown div:focus {
             background: rgba(0, 0, 0, 0.1);
         }
 
-        .nav-item:hover, .nav-item:focus-visible, .nav-item.active {
+        .nav-item:hover, .nav-item:focus, .nav-item.active {
             color: var(--accent-color);
             /* outline: 1px solid var(--accent-color); */
             outline-offset: 7px;
-        }
-
-        .nav-item:hover .icon,
-        .nav-item:focus-visible .icon,
-        .nav-item:active .icon {
-            background: transparent;
-            border: none;
-            outline: none !important;
-        }
-        .nav-item:focus-visible .icon {
-            outline: inherit; /*this makes it still visible fot tabbing*/
         }
 
         .date-box {
@@ -297,14 +286,8 @@ if (date("H:i:s") > "18:19:59") {
             width: 47px;
             height: 47px;
             /*background: #292D32;*/
-            border: none;
-            background: transparent;
-            margin: 0;
-            justify-content: center;
-            align-items: center;
-            display: flex;
+            border-radius: 50%;
         }
-        
 .nav-buttons {
     position: absolute;
     bottom: 10%; /* Adjust as needed */
@@ -341,7 +324,7 @@ if (date("H:i:s") > "18:19:59") {
 
 /* Expand button on hover */
 .nav-button:hover,
-.nav-button:focus-visible {
+.nav-button:focus {
     width: 160px;
     padding: 6px 8px;
     padding-top: 10px
@@ -354,7 +337,7 @@ if (date("H:i:s") > "18:19:59") {
 }
 
 .nav-button:hover .text,
-.nav-button:focus-visible .text {
+.nav-button:focus .text {
     opacity: 1;
     transform: translateX(0);
 }
@@ -366,7 +349,7 @@ if (date("H:i:s") > "18:19:59") {
 }
 
 .nav-button:hover .arrow,
-.nav-button:focus-visible .arrow {
+.nav-button:focus .arrow {
     transform: translateX(5px);
 }
        /* Button Control */
@@ -385,7 +368,7 @@ if (date("H:i:s") > "18:19:59") {
         }
 
         .arrow-button:hover,
-        .arrow-button:focus-visible {
+        .arrow-button:focus {
             transform: translateX(5px); /* Moves the arrow slightly on hover */
             background: transparent;
         }
@@ -426,7 +409,7 @@ if (date("H:i:s") > "18:19:59") {
         }
 
         .social-icons a:hover,
-        .social-icons a:focus-visible {
+        .social-icons a:focus {
             color: rgb(31,31,33);
         }
 
@@ -461,7 +444,7 @@ if (date("H:i:s") > "18:19:59") {
         }
 
         .footer a:hover,
-        .footer a:focus-visible {
+        .footer a:focus {
             background: rgba(255, 255, 255, 0.1);
             color: #dcdcdc;
         }
@@ -497,13 +480,13 @@ if (date("H:i:s") > "18:19:59") {
             transition: filter 0.05s, background-color 0.05s;
         }
 
-        .nav-item:hover img, .nav-item:focus-visible img, .nav-item.active img {
+        .nav-item:hover img, .nav-item:focus img, .nav-item.active img {
             filter: invert(68%) sepia(66%) saturate(345%) hue-rotate(43deg) brightness(90%) contrast(95%);
         }
        
-        .nav-item .dropdown{
+        .icon .dropdown{
             top: 130%;
-            left: -315%;
+            left: -415%;
         }
 
         .in-nav {
@@ -530,7 +513,7 @@ if (date("H:i:s") > "18:19:59") {
     }
 
     .icon-butt:hover svg,
-    .icon-butt:focus-visible svg {
+    .icon-butt:focus svg {
         transform: scale(1.1) rotate(5deg); /* Slight enlarge & tilt effect */
         fill: var(--accent-color); /* Changes to a blue shade */
     }
@@ -681,12 +664,12 @@ if (date("H:i:s") > "18:19:59") {
         <div class="right-section">
             <div class="nav-links">
                 <div class="nav-item">
-                    <button class="icon">
+                    <div class="icon">
                         <img src="images/usaicon.png" alt="User Icon" class="icon-img in-nav-img">
-                    </button>
-                    <div class="dropdown">
-                        <a href="signup.php" class="dropdown-link"><div>Create Account</div></a>
-                        <a href="login.php" class="dropdown-link"><div>Log in</div></a>
+                        <div class="dropdown">
+                            <a href="signup.php" class="dropdown-link"><div>Create Account</div></a>
+                            <a href="login.php" class="dropdown-link"><div>Log in</div></a>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -923,12 +906,12 @@ if (date("H:i:s") > "18:19:59") {
             </a>-->
             <div class="nav-links">
                 <div class="nav-item">
-                    <button class="icon">
+                    <div class="icon">
                         <img src="images/usaicon.png" alt="User Icon" class="icon-img in-nav-img">
-                    </button>
-                    <div class="dropdown">
-                        <a href="changePassword.php" class="dropdown-link"><div>Change Password</div></a>
-                        <a href="logout.php" class="dropdown-link"><div>Log Out</div></a>
+                        <div class="dropdown">
+                            <a href="changePassword.php" class="dropdown-link"><div>Change Password</div></a>
+                            <a href="logout.php" class="dropdown-link"><div>Log Out</div></a>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -994,15 +977,15 @@ if (date("H:i:s") > "18:19:59") {
             </a>
             <div class="date-box"></div>
             <div class="nav-links">
-                <div class="nav-item">
-                    <button class="icon">
+                <div class="nav-item" style="outline:none;">
+                    <div class="icon">
                         <img src="images/usaicon.png" alt="User Icon" class="icon-img in-nav-img">
-                    </button>
-                    <div class="dropdown">
-                        <a href="viewProfile.php" style="text-decoration: none;"><div>View Profile</div></a>
-                        <a href="editProfile.php" style="text-decoration: none;"><div>Edit Profile</div></a>
-                        <a href="changePassword.php" style="text-decoration: none;"><div>Change Password</div></a>
-                        <a href="logout.php" style="text-decoration: none;"><div>Log Out</div></a>
+                        <div class="dropdown">
+                            <a href="viewProfile.php" style="text-decoration: none;"><div>View Profile</div></a>
+                            <a href="editProfile.php" style="text-decoration: none;"><div>Edit Profile</div></a>
+                            <a href="changePassword.php" style="text-decoration: none;"><div>Change Password</div></a>
+                            <a href="logout.php" style="text-decoration: none;"><div>Log Out</div></a>
+                        </div>
                     </div>
                 </div>
             </div>
