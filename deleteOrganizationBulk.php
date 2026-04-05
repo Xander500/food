02d8b755bd6d9d0dc7f36a795e8392fab77282ff
mustafa <mustafa21adnan@gmail.com>
2026-04-05 18:17:18 -0400

    Implemented bulk delete for organizations with UI improvements

---
+++ deleteOrganizationBulk.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+
+if ($_SESSION['access_level'] < 2) {
+    header('Location: index.php');
+    exit;
+}
+
+require_once('database/dbOrganizations.php');
+
+$deletedCount = 0;
+$failedCount = 0;
+
+if (isset($_POST['bulk_delete']) && isset($_POST['selected_orgs'])) {
+    $selected = $_POST['selected_orgs'];
+
+    if (is_array($selected)) {
+        foreach ($selected as $id) {
+            try {
+                if (delete_organization($id)) {
+                    $deletedCount++;
+                } else {
+                    $failedCount++;
+                }
+            } catch (mysqli_sql_exception $e) {
+                $failedCount++;
+            }
+        }
+    }
+}
+
+header('Location: organizationManagement.php?deleted=' . $deletedCount . '&failed=' . $failedCount);
+exit;
+?>
