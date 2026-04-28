e403f5e0e598ef58ce5627fca5d753a1ebf3bbd0
riderle <lrider@mail.umw.edu>
2026-04-28 16:22:04 -0400

    Navbar fixed header-link tag from home-link typo

--- header.php
+++ header.php
@@ -807,7 +807,7 @@ if (date("H:i:s") > "18:19:59") {
                     </div>
                 </div>
                 <div class="nav-item">
-                    <a class="home-link" href="howto.php">Instructions</a>
+                    <a class="header-link" href="howto.php">Instructions</a>
                 </div>
             </div>
         </div>
@@ -867,7 +867,7 @@ if (date("H:i:s") > "18:19:59") {
     </div>
 </div>
                 <div class="nav-item">
-                    <a class="home-link" href="howto.php">Instructions</a>
+                    <a class="header-link" href="howto.php">Instructions</a>
                 </div>
             </div>
         </div>
